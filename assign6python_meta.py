28a9d93d26503f39710c366a036f3e22bc854948
Bushra Saeed <bushrasaeed036@gmail.com>
2025-07-06 19:56:28 +0500

    Add files via upload

---
+++ assign6python_meta.py
@@ -0,0 +1,40 @@
+# TASK 1: CHARACTERS
+char1 = "A"
+char2 = "7"
+char3 = "#"
+print(char1, char2, char3) # it will print characters in one line.
+
+
+# TASK 2: STRINGS
+animal = "Lion"
+about_animal = "Lion is the king of jungle."
+print(animal)
+print(about_animal)
+
+
+# TASK 3: CONCATENATION
+first_name = "Bushra"
+last_name = "Saeed"
+print(first_name +" "+ last_name) # it will print names with space between them.
+
+
+# TASK 4: REPETITION
+str1 = "Hi"
+print(str1*5) # it will repeat the given string 5 times.
+
+
+# TASK 5: INDEXING
+str2 = "PythonRocks"
+print(str2[0]) # it will print 1st letter.
+print(str2[3]) # it will print 4th letter.
+print(str2[-1]) # it will print last letter.
+
+
+# TASK 6: SLICING
+str3 = "I love coding in python"
+print(str3[2:6]) # it will print "love"
+print(str3[17:]) # it will print "python"
+
+# BONUS TASK
+name = "Muntaha"
+print("Welcome" +" "+ "home" +" "+ name + "!"*2) # use of concatenation and repeatetion.
